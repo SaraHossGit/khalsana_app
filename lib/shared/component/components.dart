a5eaff7f5c1196bcaf97663f72d369358c13d9ab
DELL <s.hossam@nu.edu.eg>
2022-04-04 13:33:19 +0200

    Dashboard Tiles

--- lib/shared/component/components.dart
+++ lib/shared/component/components.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 import '../../style/colors.dart';
 
-Widget defaultTextForm({
+Widget customizedTextForm({
   required var controller,
   required IconData prefixIcon,
   required String hint,
@@ -57,3 +59,85 @@ Widget defaultSearchBar() => Container(
         ),
       ),
     );
+
+Widget customizedNavBar({
+  required var selectedIndex,
+  required var onItemTapped,
+}) =>
+    Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: buttonColor2.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 12,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        child: BottomNavigationBar(
+          unselectedItemColor: buttonColor2.withOpacity(0.3),
+          selectedItemColor: buttonColor2,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'Deals',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Reminders',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+        ),
+      ),
+    );
+
+Widget customizedFloatingActionButton() => Transform.rotate(
+      angle: 40.05,
+      child: FloatingActionButton(
+        foregroundColor: backgroundColor, //Color of the "add" icon
+        splashColor: buttonColor2, //Color when the icon is pressed
+        onPressed: () {},
+        child: Transform.rotate(
+            angle: 40.05,
+            child: const Icon(
+              Icons.add,
+              size: 35.0,
+            )),
+        backgroundColor: buttonColor1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+Widget customizedHeadline({
+  required String data,
+}) =>
+    Column(children: [
+      SizedBox(
+        height: 15.0,
+      ),
+      Text(
+        data,
+        style: TextStyle(
+          color: TextColor1,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+      ),
+      SizedBox(
+        height: 15.0,
+      ),
+    ]);
--- lib/view/dealsDashboard.dart
+++ lib/view/dealsDashboard.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 import 'package:khalsana_app/style/colors.dart';
-
 import '../shared/component/components.dart';
 
 class DealsDashboard extends StatefulWidget {
@@ -12,200 +11,261 @@ class DealsDashboard extends StatefulWidget {
 
 class _DealsDashboardState extends State<DealsDashboard> {
   var searchController = TextEditingController();
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
+  int selectedIndex = 0;
+  void onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_literals_to_create_immutables
     return Scaffold(
-      floatingActionButton: Transform.rotate(
-        angle: 40.05,
-        child: FloatingActionButton(
-          foregroundColor: backgroundColor, //Color of the "add" icon
-          splashColor: buttonColor2, //Color when the icon is pressed
-          onPressed: () {},
-          child: Transform.rotate(
-              angle: 40.05,
-              child: const Icon(
-                Icons.add,
-                size: 35.0,
-              )),
-          backgroundColor: buttonColor1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-        ),
-      ),
+      floatingActionButton: customizedFloatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Container(
-        decoration: boxDecoration(),
-        // margin: const EdgeInsets.all(12.0),
-        child: ClipRRect(
-          // borderRadius:
-          // const BorderRadius.vertical(bottom: Radius.circular(25.0)),
-          child: BottomNavigationBar(
-            unselectedItemColor: buttonColor2.withOpacity(0.3),
-            selectedItemColor: buttonColor2,
-            type: BottomNavigationBarType.fixed,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_rounded),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.message),
-                label: 'Deals',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                label: 'Reminders',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_rounded),
-                label: 'Profile',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          ),
-        ),
+      bottomNavigationBar: customizedNavBar(
+        onItemTapped: onItemTapped,
+        selectedIndex: selectedIndex,
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(
             20.0,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 22.0,
-              ),
-              Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Text(
-                    "Deals Dashboard",
-                    style: TextStyle(
-                      color: TextColor1,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 60.0,
-                  ),
-                  Expanded(
-                    child: CircleAvatar(
-                      radius: 25.0,
-                      backgroundImage: AssetImage(
-                        'images/user.png',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: defaultSearchBar(),
-                  ),
-                  SizedBox(
-                    width: 5.0,
-                  ),
-                  Icon(
-                    Icons.camera_enhance_outlined,
-                    color: TextColor1,
-                    size: 38.0,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              const Text(
-                "Top Deals",
-                style: TextStyle(
-                  color: TextColor1,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 22.0,
                 ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      width: 150.0,
-                      height: 230.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        border: Border.all(
-                          color: TextColor1,
-                          width: 3,
+
+                // Title + Profile Pic.
+                Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Text(
+                      "Deals Dashboard",
+                      style: TextStyle(
+                        color: TextColor1,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60.0,
+                    ),
+                    Expanded(
+                      child: CircleAvatar(
+                        radius: 25.0,
+                        backgroundImage: AssetImage(
+                          'images/user.png',
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 5.0,
-                  ),
-                  Column(
-                    children: [
-                      Container(
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+
+                // Search Bar + Scanner
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: defaultSearchBar(),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Icon(
+                      Icons.camera_enhance_outlined,
+                      color: TextColor1,
+                      size: 38.0,
+                    ),
+                  ],
+                ),
+
+                // Top Deals Section
+                customizedHeadline(data: "Top Deals"),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
                         width: 150.0,
-                        height: 108.0,
+                        height: 230.0,
                         decoration: BoxDecoration(
-                          color: Color(0xFFFFA69E),
                           borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Image(
-                          height: 60.0,
-                          image: AssetImage(
-                            'images/med1.png',
+                          border: Border.all(
+                            color: TextColor1,
+                            width: 3,
                           ),
                         ),
                       ),
-                      SizedBox(height: 15.0),
-                      Container(
-                        width: 150.0,
-                        height: 108.0,
-                        decoration: BoxDecoration(
-                          color: buttonColor1,
-                          borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: 150.0,
+                          height: 108.0,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFFA69E),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Image(
+                            height: 60.0,
+                            image: AssetImage(
+                              'images/med1.png',
+                            ),
+                          ),
                         ),
+                        SizedBox(height: 15.0),
+                        Container(
+                          width: 150.0,
+                          height: 108.0,
+                          decoration: BoxDecoration(
+                            color: buttonColor1,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
+                // Medicines Section
+                customizedHeadline(data: "Medicines"),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  physics: BouncingScrollPhysics(),
+                  child: Row(
+                    children: [
+                      productItem(
+                        borderColor: TextColor1,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: buttonColor2,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: TextColor1,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: buttonColor2,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
                       ),
                     ],
                   ),
-                ],
-              ),
-            ],
+                ),
+
+                // Medical Equipment Section
+                customizedHeadline(data: "Medical Equipment"),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  physics: BouncingScrollPhysics(),
+                  child: Row(
+                    children: [
+                      productItem(
+                        borderColor: TextColor1,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: buttonColor2,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: TextColor1,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      productItem(
+                        borderColor: buttonColor2,
+                        productImage: 'images/med1.png',
+                        productName: "Panadol Cold & Flu",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  BoxDecoration boxDecoration() {
-    return BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: buttonColor2.withOpacity(0.3),
-          spreadRadius: 2,
-          blurRadius: 12,
+  Widget productItem({
+    required Color borderColor,
+    required String productImage,
+    required String productName,
+  }) =>
+      Container(
+        width: 150.0,
+        height: 135.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          border: Border.all(
+            color: borderColor,
+            width: 3,
+          ),
         ),
-      ],
-    );
-  }
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Image(
+                height: 65.0,
+                image: AssetImage(
+                  productImage,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                productName,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
